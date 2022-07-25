52f7973995cb31add0cef1ab41dd347cfd096ff4
Millat070 <109919285+Millat070@users.noreply.github.com>
2022-07-24 22:15:34 -0400

    first commit

---
+++ src/test/java/Javabasic/Class.java
@@ -0,0 +1,85 @@
+package Javabasic;
+
+public class Class {
+
+	
+	
+	public static void test () {
+		System.out.println("im test method");
+		
+		
+	}; 
+	
+	public static void Apple ()
+	{System.out.println("i am Steve Jobs");	
+	}	
+	
+	
+	
+	
+	public static void Java ()
+	{System.out.println("Horrible Java");	
+	}
+	
+	
+	
+	public static void Nike ()
+	{System.out.println("Just do it");	
+	}
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey");
+		
+	test();
+	Apple();
+	Java();
+	Nike();
+	
+	 
+	}
+
+	
+	
+	public static int total sum (int x, int y){
+	
+		int sum = x+y;
+	 //System.out.println("i am inside method:"+sum)	
+	return sum;  
+		
+	}
+	 
+	
+	
+	
+	public static void main (string){} arg){
+	
+		int y=300000000;
+		long ph=00000000000000000000000000000000000000000;
+		string yy= "Millat";
+		
+		System.out.println(totalSum)(3,5));
+	}
+		
+		
+		
+		
+		
+	
+	
+	}  
+	 
+	
+	
+	
+	
+	
+	
+	
+}
